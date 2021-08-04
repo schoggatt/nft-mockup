c45df0cb17f8d5d43d50537051305e3eebce4640
schoggatt <samuelchoggatt@gmail.com>
2021-08-03 22:24:31 -0500

    changed layering of slideshow

--- src/frontend/components/SlideShow.js
+++ src/frontend/components/SlideShow.js
@@ -51,18 +51,18 @@ const SlideShow = ({slides}) => {
             className={index === current ? 'slide active' : 'slide'}
             key={index}
           >
-            <button className='button-left' onClick={prevSlide}>
-              <img className='slideshow-button-left' src='https://dsm01pap001files.storage.live.com/y4mhylUSU2K7udh7_8fwRh7gHNk7YFf0vDA5hhjJERoHnWL05L5JWYM8oyC2oNrvL0-8KfmWn2o4cGc9w9DkSWiF3vq4ubieMhxxyFhxnP0NkbxQunXCtl7BB-XexOhmvP3sVhjxaitMugq344YJDZkUzv69GNWML17Uhz2KkPS_82voTMGdu6KON-j3RorsFDr?width=50&height=50&cropmode=none'/>
-            </button>
-            <button className='button-right' onClick={nextSlide}>
-              <img className='slideshow-button-right' src='https://dsm01pap001files.storage.live.com/y4mxKaPjzbjAgSpF471mHt05zM0HEauXpNdRkLrF_LxKlJQ-HypFZdAdCSFvBCm6NqQnxWJ7W3pd_zuI8nl-ZqKy-YFyjDFgBHdpY-zQn3266pHw3uM8CFcMvzAV3hmhD7saEy2cE2ANEhSYqzFfY44YAivxGZK-qeXpAiZ2htN3DX0w0gvdiHFkKYL2lfRzJ5I?width=50&height=50&cropmode=none'/>
-            </button>
             {index === current && (
               <img src={slide.image} className='image' />
             )}
             <div class="overlay">
                 <img src="https://dsm01pap001files.storage.live.com/y4mbFwwGCedqUGsg-49TEpfVWrM-rqoyXA7zE5qGKNX93SwMkxvFi_m5oEiqNK6yCeiZR0Yiws5doJNzJAzQlePC939Rele0HxydpfmIAH4vIDSeC7lozakIF4VhZzp_ccGQwGjFOouauQ12eNZBeLVwjhhbRncKcFUqfF4AZES8dPvjQKDUtQq7DbecQdZMEar?width=1920&height=1080&cropmode=none" className='overlay-image'/>
             </div>
+            <button className='button-left' onClick={prevSlide}>
+              <img className='slideshow-button-left' src='https://dsm01pap001files.storage.live.com/y4mhylUSU2K7udh7_8fwRh7gHNk7YFf0vDA5hhjJERoHnWL05L5JWYM8oyC2oNrvL0-8KfmWn2o4cGc9w9DkSWiF3vq4ubieMhxxyFhxnP0NkbxQunXCtl7BB-XexOhmvP3sVhjxaitMugq344YJDZkUzv69GNWML17Uhz2KkPS_82voTMGdu6KON-j3RorsFDr?width=50&height=50&cropmode=none'/>
+            </button>
+            <button className='button-right' onClick={nextSlide}>
+              <img className='slideshow-button-right' src='https://dsm01pap001files.storage.live.com/y4mxKaPjzbjAgSpF471mHt05zM0HEauXpNdRkLrF_LxKlJQ-HypFZdAdCSFvBCm6NqQnxWJ7W3pd_zuI8nl-ZqKy-YFyjDFgBHdpY-zQn3266pHw3uM8CFcMvzAV3hmhD7saEy2cE2ANEhSYqzFfY44YAivxGZK-qeXpAiZ2htN3DX0w0gvdiHFkKYL2lfRzJ5I?width=50&height=50&cropmode=none'/>
+            </button>
           </div>
         );
       })}
